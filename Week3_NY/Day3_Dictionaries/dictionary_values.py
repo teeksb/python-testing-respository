136a1c096fb0fc37af852c43d32a9b5ce39314e6
Tosin Kasaba <113717712+teeksb@users.noreply.github.com>
2026-01-07 12:47:05 -0700

    feat: dictionaries

---
+++ Week3_NY/Day3_Dictionaries/dictionary_values.py
@@ -0,0 +1,10 @@
+car = dict(
+    brand = "Ford",
+    model = "Mustang",
+    engineLitre = 5.0,
+    transmission = "manual"
+)
+
+print("********************")
+# You access values in a dictionary using a square bracket and the name of the key as a string within the brackets.
+print(car["brand"])
---
+++ Week3_NY/Day3_Dictionaries/intro.py
@@ -0,0 +1,43 @@
+# A dictionary is a data type used to store data values in a key value pair.
+# Values in a dictionary can be of any datat type.
+# Keys must be strings.
+# A dictionary is an ordered collection(as from Version 3.17) which is changeable/mutable and ordered.
+# It doesn't allow duplicates
+# Keys must be unique
+
+from pprint import pprint
+
+
+student_a = {
+    "first_name": "Tosin",
+    "last_name" : "Kasaba",
+    "age" : 20,
+    "height" : 1.65,
+    "gender" : "Male",
+    "registered": True,
+    "skills": []
+}
+
+recent_orders = {
+    "orderID": 2345666,
+    "orderDate": 4554,
+    "shipMode": 4554,
+    "customerName": 4556,
+}
+
+pprint(student_a)
+print(student_a["first_name"])
+
+
+print("*************************")
+# Create dictionary using dict() constructor
+student_b = dict(
+    first_name = "Tosin",
+    last_name = "Kasaba",
+    age = 20,
+    height = 20,
+    gender = "Male",
+)
+
+pprint(student_b)
+print(student_b["first_name"])

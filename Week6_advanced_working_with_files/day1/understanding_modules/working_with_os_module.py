d5d148dda458c557dc3fdbc09412eb8bca13460c
Tosin Kasaba <113717712+teeksb@users.noreply.github.com>
2026-01-27 01:24:58 -0700

    feat: "os module"

--- Week6_advanced_working_with_files/day1/understanding_modules/working_with_os_module.py
+++ Week6_advanced_working_with_files/day1/understanding_modules/working_with_os_module.py
@@ -1,4 +1,5 @@
-import os 
+import os
+from pprint import pprint
 
 # File and directory operations
 # Process maanagement
@@ -10,3 +11,22 @@ cwd = os.getcwd()
 print("My current working directory is:")
 print(cwd)
 print("")
+
+
+cwd_content = os.listdir()
+print(cwd_content)
+print("")
+
+cwd_content.sort()
+pprint(cwd_content)
+print("")
+
+# To know if a file exists
+if os.path.exists("C:\\Users\Jubril\Documents\Python_Training\Week5_CSV2\day1_working_with_files\Files"):
+    print('example.txt found!')
+else:
+    print("Cannot find example.txt!")
+
+
+if not os.path.exists("Sample Directory"):
+    os.mkdir("Sample Directory")
